44540d1cb50c9fc8fa2a91a672ceb2558ce6d238
Paras <paraskamliya5@gmail.com>
2023-09-30 15:29:17 +0530

    chakra ui update

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
 import { BrowserRouter } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </Provider>
 
 )
