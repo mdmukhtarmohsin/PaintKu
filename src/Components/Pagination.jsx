78284f627fa235eee3d9aa03e8b38484c48ef59f
Paras <paraskamliya5@gmail.com>
2023-09-30 20:03:38 +0530

    done

---
+++ src/Components/Pagination.jsx
@@ -0,0 +1,28 @@
+import { Button, Center, HStack } from "@chakra-ui/react"
+
+const Pagination = ({ totalData, handlePageChange, page, limit }) => {
+    const totalPage = Math.ceil(totalData / limit);
+    const renderButtons = () => {
+        const buttons = [];
+        for (let i = 1; i <= totalPage; i++) {
+            buttons.push(
+                <Button
+                    key={i}
+                    border={"1px"}
+                    bg={i === page ? "#4AAB76" : "green.50"}
+                    color={i === page ? "white" : "black"}
+                    onClick={() => handlePageChange(i)}
+                >
+                    {i}
+                </Button>
+            );
+        }
+        return buttons;
+    };
+    return <Center mt={10}>
+        <HStack>
+            {renderButtons()}
+        </HStack>
+    </Center>
+}
+export default Pagination
