40db5aefeb1d20663fc2f8f669dfb34263efde16
Paras <paraskamliya5@gmail.com>
2023-10-01 09:03:54 +0530

    done

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -2,7 +2,7 @@ import { Box, Image, Text } from "@chakra-ui/react"
 
 const ProductCard = ({ image, title, price, rating, brand }) => {
     return <Box justifyContent={"center"} >
-        <Image src={image} m={"auto"} h={"400px"} w={"auto"} />
+        <Image src={image} m={"auto"} h={"350px"} w={"auto"} />
         <Text noOfLines={1} fontSize={"xl"} fontWeight={500}>{title}</Text>
         <Text fontSize={"md"} fontWeight={400}>₹ {price}</Text>
         <Text fontSize={"md"} fontWeight={400}>{rating} ⭐</Text>
