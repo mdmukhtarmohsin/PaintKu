b19a28dcab681f2955c8ae729f586b5f27382fc0
JahirPendhari <jahirpp1999@gmail.com>
2023-10-02 09:38:07 +0530

    Single Prodcut Page done

--- src/Components/SingleProduct.jsx
+++ src/Components/SingleProduct.jsx
@@ -23,13 +23,18 @@ const SingleProduct = () => {
     return <DIV>
         <div className="box-1">
             <img src={singleData.image}/>
+            <div className="btn">
+                <button>ADD TO CART</button>
+                <button>BUY NOW</button>
+            </div>
         </div>
         <div className="box-2" >
-            <p style={{fontSize:"35px", marginBottom:"30px",color:"blue"}}>{singleData.title}</p>
+            <p style={{fontSize:"35px", marginBottom:"10px",color:"blue"}}><b>{singleData.title}</b></p>
             <p style={{fontSize:"20px",}}>Brand : {singleData.brand}</p>
-            <p style={{fontSize:"20px"}}>Price : ${singleData.price}.00</p>
+            <p style={{fontSize:"16px",color:"green"}}>Special Price </p>
+            <p style={{fontSize:"30px",fontWeight:"revert"}}> ₹ {singleData.price}.00</p>
             <div style={{display:"flex",justifyContent:"space-between"}}>
-                <p>Rating : </p>
+                <p style={{color:(singleData.rating) >=4? "green":"red"}}>Rating : </p>
                <DIV1>
                 {
                     Array(5).fill(1).map((item,i)=>{
@@ -38,6 +43,9 @@ const SingleProduct = () => {
                 }
                </DIV1>
             </div>
+            <p><b>Available offers</b></p>
+            <p>🏷️ Bank Offer 5% off on Axis Bank Credit Card, up to ₹625, on orders of ₹5,000 and above <span style={{color:"blue",cursor:"pointer"}}>T&C</span></p>
+            <p>🏷️ Special PriceGet extra 12% off (price inclusive of cashback/coupon)  <span style={{color:"blue",cursor:"pointer"}}>T&C</span></p>
             
         </div>
     </DIV>
@@ -47,7 +55,7 @@ export default SingleProduct;
 
 const DIV=styled.div`
     width: 100%;
-    /* text-align: center; */
+    /* text-align: left; */
     display: flex;
     margin-top:50px;
     overflow: hidden;
@@ -56,25 +64,50 @@ const DIV=styled.div`
     /* border: 1px solid red; */
 
     .box-1{
-        width: 60%;
+        width: 50%;
         border-radius: 30px;
         box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
-        padding: 50px;
+        padding: 30px;
+        display: flex;
+        flex-direction: column;
+        background-color:lightcyan;
     }
     img{
         width: 100%;
         height: 300px;
         border-radius:20px;
+        cursor: pointer;
+    }
+    .btn{
+        display: flex;
+        justify-content: space-between;
+        margin-top:20px;
+    }
+    button{
+        width:45%;
+        background-color:orange;
+        color: white;
+        font-weight:600;
+        height:50px ;
+        padding: 10px;
+        border-radius: 5px;
+    }
+    button:nth-child(1){
+        background-color:orange;
+    }
+    button:nth-child(2){
+        background-color:red;
     }
 
     .box-2{
         width: 100%;
-        text-align: center;
+        text-align: left;
         padding:20px;
         display: flex;
         flex-direction: column;
-        align-items:center;
-        gap:15px;
+        align-items: start;
+        margin-left:30px;
+        gap:10px;
     }
 
 `
